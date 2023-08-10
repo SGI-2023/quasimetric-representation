ba220bffd55308013ea887b9bc9dfe52e7f14fd8
danperazzo <drp@cin.ufpe.br>
2023-08-10 11:46:35 -0300

    [ADDED] Cleaned code for visualization

--- testing_checkpoint.py
+++ testing_checkpoint.py
@@ -9,11 +9,11 @@ from quasimetric_rl.modules import QRLAgent, QRLConf
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
+import numpy as np
  
-fig, ax = plt.subplots(1, 1)
-fig.set_size_inches(5,5)
+from visualize_trajectories import visualize_trajectory
 
-expr_checkpoint = 'offline/results/d4rl_custom-grid-tank-goal-v1/iqe(dim=2048,components=64)_dyn=1_seed=60912/checkpoint_00023_00000_final.pth'  # FIXME
+expr_checkpoint = 'offline/results/d4rl_custom-grid-tank-goal-v1/iqe(dim=2048,components=64)_dyn=1_seed=60912/checkpoint_00210_00000_final.pth'  # FIXME
 
 expr_dir = os.path.dirname(expr_checkpoint)
 with open(expr_dir + '/config.yaml', 'r') as f:
@@ -37,22 +37,36 @@ agent: QRLAgent = agent_conf.make(env_spec=dataset.env_spec, total_optim_steps=1
 
 # 3. Load checkpoint
 agent.load_state_dict(torch.load(expr_checkpoint, map_location='cpu')['agent'])
-
+actions = torch.tensor([0, 1, 2])  
 critic= agent.critics[0]
 
 # greedy 1-step planning
-obs = torch.tensor([0.5, 0.5, 0, 1.5, 1.5 ])  # current state vector
-goal_obs = torch.tensor([1.5, 1.5, 0, 1.5, 1.5 ])
+observation = env.reset()
+goal_obs = torch.tensor([2*np.pi*0.8, 2*np.pi*0.8, 0 ], dtype=torch.float32)
+trajectory_points = []
 
-actions = torch.tensor([0, 1, 2])  
+done = False
+for i in range(1000):
+  
+  observation_tensor = torch.tensor(observation,  dtype=torch.float32)
+  trajectory_points.append(observation)
+  if done:
+    break 
+  
+  distances = critic(
+      observation_tensor[None, :],  
+      goal_obs[None, :],
+      action=actions,   
+  )  
+  
+  best_action = distances.argmax(dim=0)  
+  print(best_action)
+  observation, _, done, _ = env.step(best_action)
 
-distances = critic(
-    obs[None, :],  
-    goal_obs[None, :],
-    action=actions,   
-)  
-best_action = distances.argmin(dim=0)  
-print(best_action)
+  max_value = distances.max(dim=0)
 
+  print(f'Iteration {i} and estimated {max_value} and observation {observation} and action {best_action}')
 
-# 4 Test trajectory
+trajectory_points = np.array(trajectory_points)
+
+visualize_trajectory(trajectory_points,env, name="agent_trajectory.gif")
--- visualize_trajectories.py
+++ visualize_trajectories.py
@@ -3,44 +3,49 @@ import numpy as np
 from quasimetric_rl.data.d4rl.grid_tank_goal import Tank_reach_goal
 import matplotlib.pyplot as plt
 
-env = Tank_reach_goal()
-
-name = 'trajectories_custom/test_0970.npz'
-
-dict_episode = np.load(name)
-
-trajectory_points = dict_episode['observations']
-num_frames = len(trajectory_points)
-
-fig, ax = plt.subplots(1, 1)
-fig.set_size_inches(5,5)
-
-
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
 
-def animate(i):
-    ax.clear()
-    # Get the point from the points list at index i
-    observation = trajectory_points[i,:]
-    point = observation[0:2]
+def visualize_trajectory(trajectory_points, env,  name = "visualize_trajectory.gif"):
+    num_frames = len(trajectory_points)
 
-    plt.xlim([0, env.size+1])
-    plt.ylim([0, env.size+1])
+    fig, ax = plt.subplots(1, 1)
+    fig.set_size_inches(5,5)
 
-    # Plot that point using the x and y coordinates
-    ax.plot(point[0], point[1], color='green', 
-            label='original', marker='o')
-    
-    goal_point = env.goal
-    ax.plot(goal_point[0], goal_point[1], color='red', 
-            label='original', marker='o')
+    def animate(i):
+        ax.clear()
+        # Get the point from the points list at index i
+        observation = trajectory_points[i,:]
+        point = observation[0:2]
+
+        plt.xlim([0, env.size+1])
+        plt.ylim([0, env.size+1])
+
+        # Plot that point using the x and y coordinates
+        ax.plot(point[0], point[1], color='green', 
+                label='original', marker='o')
+        
+        goal_point = env.goal
+        ax.plot(goal_point[0], goal_point[1], color='red', 
+                label='original', marker='o')
 
 
 
-ani = FuncAnimation(fig, animate, frames=num_frames,
-                    interval=1, repeat=False)
+    ani = FuncAnimation(fig, animate, frames=num_frames,
+                        interval=1, repeat=False)
 
-ani.save("simple_animation.gif", dpi=300,
-         writer=PillowWriter(fps=30))
-plt.close()
+    ani.save(name, dpi=300,
+            writer=PillowWriter(fps=30))
+    plt.close()
+
+
+if __name__ == "__main__":
+
+    env = Tank_reach_goal()
+
+    name = 'trajectories_custom/test_0970.npz'
+
+    dict_episode = np.load(name)
+
+    trajectory_points = dict_episode['observations']
+    visualize_trajectory(trajectory_points, env)
